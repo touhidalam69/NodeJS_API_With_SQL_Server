4b9ad29670dd7303da61e7ce282cec2b1d6e37b7
Md. Touhid Alam <touhidalam69@gmail.com>
2021-03-24 18:08:49 +0600

    Update Connect.js

--- connection/Connect.js
+++ connection/Connect.js
@@ -2,7 +2,7 @@ var sql = require("mssql");
 var connect = function () {
     var conn = new sql.ConnectionPool({
         user: 'sa',
-        password: 'touhid',
+        password: 'touhid123',
         server: '192.168.1.138\\SQL2014',
         database: 'TESTDB',
         port: 1433,
